b7e543306e26f1efa36ff6347ee1215403590b7b
nck.974 <nck.974@gmail.com>
2024-08-11 12:04:39 +0200

    Add how many tracks are pending to be downloaded

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, signal, WritableSignal } from '@angular/core';
 import * as L from 'leaflet';
 import { TrackService } from '../track.service';
 import { Coordinate } from '../model/coordinate';
@@ -16,6 +16,9 @@ export class MapComponent implements AfterViewInit {
   private map!: L.Map;
   private static defaultLocation: L.LatLng = new L.LatLng(49.4521, 11.0767);
   private displayedTracks: L.Polyline[] = []
+  tracksToDownload: WritableSignal<number> = signal(0);
+  downloadedTracks: WritableSignal<number> = signal(0);
+  isLoadingTracks = false;
 
   constructor(private trackService: TrackService) { }
 
@@ -54,6 +57,12 @@ export class MapComponent implements AfterViewInit {
     this.trackService.getTrack(filename).subscribe((rawCoordinates: Coordinate[]) => {
       const coordinates = rawCoordinates.map<L.LatLng>(coordinate => new L.LatLng(coordinate.a, coordinate.o));
       this.addTrackToMap(coordinates);
+      this.downloadedTracks.update(currentValue => currentValue + 1);
+      if (this.downloadedTracks() == this.tracksToDownload()) {
+        this.isLoadingTracks = false;
+        this.downloadedTracks.set(0);
+        this.downloadedTracks.set(0);
+      }
     });
   }
 
@@ -69,9 +78,17 @@ export class MapComponent implements AfterViewInit {
     console.log("southWest");
     console.log(southWest);
 
+    this.tracksToDownload.set(0);
+    this.isLoadingTracks = true;
     this.trackService.getTracksInsideSquare(northEast, southWest).subscribe((file: FileList) => {
-      console.log(file)
-      file.fileList.forEach(filename => this.displayTrack(filename));
+
+      if (this.tracksToDownload() > 0) {
+        this.tracksToDownload.set(file.fileList.length);
+        this.downloadedTracks.set(0);
+        file.fileList.forEach(filename => this.displayTrack(filename));
+      } else {
+        this.isLoadingTracks = false;
+      }
     });
 
   }
